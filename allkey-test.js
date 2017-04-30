94b5c1388fb98caa7be6e93a998bb21bdcf985ee
volkovasystems <richeve.bebedor@gmail.com>
2017-04-30 21:06:14 +0800

    Finished allkey procedure.

---
+++ allkey-test.js
@@ -0,0 +1,21 @@
+
+const assert = require( "assert" );
+const allkey = require( "./allkey.js" );
+
+//assert.equal( allkey( "toString", null ), true, "should be true" );
+
+assert.equal( allkey( "toString", NaN ), true, "should be true" );
+
+assert.equal( allkey( "toString", undefined ), true, "should be true" );
+
+assert.equal( allkey( "toString", 123 ), true, "should be true" );
+
+assert.equal( allkey( [ "toString", "valueOf" ], 123 ), true, "should be true" );
+
+assert.equal( allkey( Symbol.for( "property" ), { [ Symbol.for( "property" ) ]: "value" } ), true, "should be true" );
+
+assert.equal( allkey( "property", { "property": "value" } ), true, "should be true" );
+
+assert.equal( allkey( [ "toString", "valueOf", "yeah" ], false ), false, "should be false" );
+
+console.log( "ok" );
